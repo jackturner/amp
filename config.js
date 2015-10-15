a643ee0ec492ecfb543cf5bcb19db00509505255
Jack Turner <jack@turner.io>
2015-10-15 15:42:16 -0400

    should never have been in repository

--- config.js
+++
@@ -1,7 +0,0 @@
-var conf = {}
-
-conf.env = process.env["NODE_ENV"] || "development"
-conf.db_password = (conf.env === "development") ? "root" : "DONT YET KNOW"
-conf.db_port = (conf.env === "development") ? 8889 : 3306
-
-module.exports = conf
